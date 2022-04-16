e6cea83ad93c5ad04b3a9ca4290dbdda274338a8
xiaoyu <yuriyshea@163.com>
2022-04-17 00:20:46 +0800

    状态模式增加注释

--- src/test/kotlin/com/xy/designpatterns/state/Main.kt
+++ src/test/kotlin/com/xy/designpatterns/state/Main.kt
@@ -2,8 +2,14 @@ package com.xy.designpatterns.state
 
 import org.junit.jupiter.api.Test
 
+/**
+ * 状态模式示例
+ */
 class Main {
 
+    /**
+     * 演示状态模式
+     */
     @Test
     fun main() {
         StatePatternSceneSimulator.run()
