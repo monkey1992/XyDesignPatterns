08d285d565d51d7db828b3928e9e4edb04173a83
xiaoyu <yuriyshea@163.com>
2022-06-26 12:06:25 +0800

    模板模式完善

--- src/main/kotlin/com/xy/designpatterns/template/TemplatePatternSceneSimulator.kt
+++ src/main/kotlin/com/xy/designpatterns/template/TemplatePatternSceneSimulator.kt
@@ -13,12 +13,15 @@ class TemplatePatternSceneSimulator : ISceneSimulator {
 
     override fun run() {
         println("============模板模式示例============")
+        println("Bird的生活：")
         val bird: AbsAnimal = Bird()
         bird.live()
         println("====================================")
+        println("Fish的生活：")
         val fish: AbsAnimal = Fish()
         fish.live()
         println("====================================")
+        println("Lion的生活：")
         val lion: AbsAnimal = Lion()
         lion.live()
         println("============模板模式示例============")
