97afa5eb9c1a6e44e957f92651714915c5f45e03
xiaoyu <yuriyshea@163.com>
2022-07-04 23:13:53 +0800

    适配器模式

--- src/main/kotlin/com/xy/designpatterns/adapter/classdiagram/classadapter/Adapter.kt
+++ src/main/kotlin/com/xy/designpatterns/adapter/classdiagram/classadapter/Adapter.kt
@@ -1,12 +1,13 @@
 package com.xy.designpatterns.adapter.classdiagram.classadapter
 
 import com.xy.designpatterns.adapter.classdiagram.ITarget
+import com.xy.designpatterns.adapter.classdiagram.objectadapter.Adapter
 
 /**
  * 适配器
  *
- * 通过继承[Adaptee]并同时实现[ITarget]，使适配器拥有同时拥有[Adaptee]和[ITarget]的能力，
- * 这样就能基于[Adaptee]现有的能力来实现目标接口定义的需求。
+ * 通过继承[Adaptee]并实现[ITarget]，使[Adapter]拥有同时拥有[Adaptee]和[ITarget]的能力，
+ * 从而将[ITarget]的具体实现转移到[Adaptee]类。
  */
 class Adapter : Adaptee(), ITarget {
 
